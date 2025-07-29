d7672bfa9b4841df8b2fe066e5741c844be8787b
Lauren Javaly <ljavaly@gmail.com>
2025-07-28 22:25:11 -0400

    Styling tweaks and add ghost

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -61,8 +61,6 @@ function Inbox() {
 
                     <div className="messages-section-header">
                         <span>Messages</span>
-                        {/* Placeholder for New Message Icon */}
-                        <svg className="w-5 h-5 text-gray-400 cursor-pointer hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clipRule="evenodd"></path></svg>
                     </div>
 
                     <div className="tabs-container">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,17 @@
 import '../styles/NavBar.css';
+import ghostIcon from '../assets/ghost-svgrepo-com.svg';
 
 function NavBar({onPageChange}) {
   return (
       <header className="header-main">
           <div className="flex items-center space-x-6">
+              {/* Ghost icon */}
+              <img 
+                src={ghostIcon} 
+                alt="Ghost" 
+                className="ghost-icon"
+              />
+              
               {/* Added onClick handlers to navigate */}
               <a href="#" className="nav-link" onClick={() => onPageChange('video')}>Discover</a>
               <a href="#" className="nav-link" onClick={() => onPageChange('following')}>Following</a>
@@ -14,13 +22,12 @@ function NavBar({onPageChange}) {
           <div className="flex-grow max-w-md mx-4">
               <div className="relative">
                   <input type="text" placeholder="Search" className="search-input"/>
-                  <svg className="search-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
               </div>
           </div>
 
           <div className="flex items-center space-x-4">
               <div className="profile-avatar">
-                  JD
+                  TR
               </div>
           </div>
       </header>
