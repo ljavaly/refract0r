760d868b4753d26ac0e2dac7abb8005404b61f7e
Lauren Javaly <ljavaly@gmail.com>
2025-09-18 00:39:10 -0400

    Add magnifying glass to search bar

--- packages/frontend/src/components/TopNavBar.jsx
+++ packages/frontend/src/components/TopNavBar.jsx
@@ -79,12 +79,28 @@ function TopNavBar({ onPageChange }) {
           </svg>
         </div>
         <div className="search-section flex-grow">
-          <input
-            id="search"
-            type="text"
-            placeholder="Search"
-            className="search-input"
-          />
+          <div className="search-input-container">
+            <input
+              id="search"
+              type="text"
+              placeholder="Search"
+              className="search-input"
+            />
+            <svg
+              className="search-icon"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
         </div>
         <div className="nav-link-container grid grid-cols-3">
           <div className="nav-link col-span-1">
